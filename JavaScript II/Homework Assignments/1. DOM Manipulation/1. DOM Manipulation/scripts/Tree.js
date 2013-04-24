67987ccb183b8e21a05936be90d8a82b2ca07508
Victor Alexiev <vic.alexiev@gmail.com>
2013-04-24 23:53:29 +0300

    bug fixes

--- JavaScript II/Homework Assignments/1. DOM Manipulation/1. DOM Manipulation/scripts/Tree.js
+++ JavaScript II/Homework Assignments/1. DOM Manipulation/1. DOM Manipulation/scripts/Tree.js
@@ -1,29 +1,6 @@
 ﻿function Tree() {
-    this.root = document.createElement("ul");
-    this.root.style.listStyleType = "none";
-
-    this.icons = ["images/blank.png", "images/collapsed.png", "images/expanded.png"];
-
-    /*
-     * Gets called during initialization or any time an image is clicked.
-     * @param {Node} node The parent 'li' of the image which has been clicked.
-     */
-    this.getIcon = function (node) {
-        // see if there are any subtrees
-        var subtrees = node.getElementsByTagName("ul");
-
-        if (subtrees.length === 0) {
-            // no icon for the nested 'ul'
-            return this.icons[0];
-        }
-
-        if (subtrees[0].style.display === "none") {
-            return this.icons[1];
-        }
-        else {
-            return this.icons[2];
-        }
-    };
+    var root = document.createElement("ul");
+    root.style.listStyleType = "none";
 
     /*
      * Adds a simple 'li' to the tree or another tree with its label specified.
@@ -32,10 +9,8 @@
      * @param {string} label The label of the subtree passed as first parameter.
      * This label will be set as the text of the parent 'li' element.
      */
-    this.addTreeItem = function (item, label) {
+    function addTreeItem(item, label) {
         var listItem = document.createElement("li");
-        // save reference to the parent
-        listItem.parent = this;
         // create an image element
         var image = document.createElement("img");
         // create a 'span' element and push the 'li' content in it (except for the nested 'ul' element).
@@ -43,65 +18,95 @@
 
         if (arguments.length === 1) {
             spanWrapper.innerHTML = item;
-        }
-        else {
+        } else {
             item.root.style.display = "none";
             listItem.appendChild(item.root);
             spanWrapper.innerHTML = label;
 
-            image.onclick = function (event) {
-                // ensure the event object is defined
-                if (!event) event = window.event;
-
-                // find the HTML element the event took place on
-                // (an image, its parent should be a 'li' element)
-                var eventSource = (event.target ? event.target : event.srcElement);
-                // call handler to process event
-                eventSource.parentNode.parent.onClickImage(eventSource.parentNode);
-            };
+            if (image.addEventListener) {
+                image.addEventListener("click", onClickImage, false);
+            } else {
+                image.attachEvent("onclick", onClickImage);
+            }
         }
 
         // assign image URL
-        image.src = this.getIcon(listItem);
+        image.src = getIcon(listItem);
         // insert the 'span' in the 'li' element
         listItem.insertBefore(spanWrapper, listItem.firstChild);
         // insert the image in the 'li' element before any other child
         listItem.insertBefore(image, listItem.firstChild);
         // add the 'li' in the tree
-        this.root.appendChild(listItem);
-    };
+        root.appendChild(listItem);
+    }
 
     /*
      * Used to visualize the tree in the HTML document.
      * @param {string} parentId The Id of the parent
      * which this tree will be appended to.
      */
-    this.setParent = function (parentId) {
+    function setParent(parentId) {
         var parent = document.getElementById(parentId);
-        parent.appendChild(this.root);
-    };
+        parent.appendChild(root);
+    }
+
+    /*
+     * Gets called during initialization or any time an image is clicked.
+     * @param {Node} node The parent 'li' of the image which has been clicked.
+     */
+    function getIcon(node) {
+        // see if there are any subtrees
+        var subtrees = node.getElementsByTagName("ul");
+
+        if (subtrees.length === 0) {
+            // no icon for the nested 'ul'
+            return "images/blank.png";
+        }
+
+        if (subtrees[0].style.display === "none") {
+            return "images/collapsed.png";
+        } else {
+            return "images/expanded.png";
+        }
+    }
 
     /*
      * Gets called when an image is clicked to expand / collapse the node.
      * @param {Node} node The parent 'li' of the image which has been clicked.
      */
-    this.onClickImage = function (node) {
+    function onClickImage(event) {
+        // ensure the event object is defined
+        if (!event) event = window.event;
+
+        // find the HTML element the event took place on
+        // (an image, its parent should be a 'li' element)
+        var eventSource = (event.target ? event.target : event.srcElement);
+
         // check if there are subtrees
-        var subtrees = node.getElementsByTagName("ul");
-        if (subtrees.length === 0) {
-            return;
+        var subtrees = eventSource.parentNode.getElementsByTagName("ul");
+        if (subtrees.length > 0) {
+            // toggle visibility of the subtree (the nested 'ul')
+            if (subtrees[0].style.display !== "none") {
+                subtrees[0].style.display = "none";
+            } else {
+                subtrees[0].style.display = "block";
+            }
+
+            var nodeImages = eventSource.parentNode.getElementsByTagName("img");
+            // change the node image.
+            nodeImages[0].src = getIcon(eventSource.parentNode);
         }
 
-        // toggle visibility of the subtree (the nested 'ul')
-        if (subtrees[0].style.display !== "none") {
-            subtrees[0].style.display = "none";
-        }
-        else {
-            subtrees[0].style.display = "block";
+        if (event.preventDefault) {
+            event.preventDefault();
         }
 
-        var nodeImages = node.getElementsByTagName("img");
-        // change the node image.
-        nodeImages[0].src = this.getIcon(node);
-    };
+        return false;
+    }
+
+    return {
+        addTreeItem: addTreeItem,
+        setParent: setParent,
+        root: root
+    }
 }

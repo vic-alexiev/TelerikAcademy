fa8b0b3577ef65b4eb48741e828f3b61d9112a1e
Victor Alexiev <vic.alexiev@gmail.com>
2013-05-14 12:15:39 +0300

    Development Tools solution added.

---
+++ High Quality Code/Homework Assignments/14. DevelopmentTools/01. VariousTools/LoggingDemo.cs
@@ -0,0 +1,34 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Reflection;
+
+internal class LoggingDemo
+{
+    private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+    private static void Main()
+    {
+        // log4net configuration is placed in the 'App.config' file where
+        // a new <log4net> section has been added.
+        // Currently two appenders are defined: the first prints the log
+        // messages to the console, the second - to the file 'DemoLogger.log'
+        // located in the application root directory (\bin\Debug).
+        XmlConfigurator.Configure();
+
+        Log.Debug("Debug message");
+        Log.Info("Info message");
+        Log.Warn("Warning message");
+        Log.Error("Error message");
+        Log.Fatal("Fatal message");
+
+        try
+        {
+            throw new InvalidOperationException("An exception occurred.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Log.Error("Error.", ex);
+        }
+    }
+}

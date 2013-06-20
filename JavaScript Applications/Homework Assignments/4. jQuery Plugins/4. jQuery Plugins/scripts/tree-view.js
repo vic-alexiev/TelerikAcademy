c504e5b4b68586763e9116b7b086d3e7c4422870
Victor Alexiev <vic.alexiev@gmail.com>
2013-06-20 18:01:25 +0300

    jQuery plugins solution added.

---
+++ JavaScript Applications/Homework Assignments/4. jQuery Plugins/4. jQuery Plugins/scripts/tree-view.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    $.fn.treeView = function () {
+
+        $(this.selector + " li").prepend("<span class='handle'></span>");
+
+        $(this.selector + " li:has(ul)").children(":first-child")
+            .addClass("collapsed").click(function () {
+                $(this).toggleClass("collapsed expanded")
+                    .siblings("ul").toggle();
+            });
+
+        return this;
+    };
+})(jQuery);

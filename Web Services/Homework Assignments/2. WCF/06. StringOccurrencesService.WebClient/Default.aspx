abafb16830aeee2ed7d45e3672136c86470a75b5
Victor Alexiev <vic.alexiev@gmail.com>
2013-08-11 17:25:07 +0300

    web client project added.

---
+++ Web Services/Homework Assignments/2. WCF/06. StringOccurrencesService.WebClient/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="StringOccurrencesService.WebClient.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="LabelSourceString" runat="server" Text="Source string:"></asp:Label>
+        <asp:TextBox ID="TextBoxSourceString" runat="server"></asp:TextBox>
+    
+    </div>
+        <asp:Label ID="LabelTargetString" runat="server" Text="Target string:"></asp:Label>
+        <asp:TextBox ID="TextBoxTargetString" runat="server"></asp:TextBox>
+        <p>
+            <asp:Button ID="ButtonCountOccurrences" runat="server" OnClick="ButtonCountOccurrences_Click" Text="Occurrences" />
+        </p>
+        <p>
+            <asp:Label ID="LabelOccurrences" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Web Services/Homework Assignments/2. WCF/06. StringOccurrencesService.WebClient/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using StringOccurrencesService.WebClient.StringOccurrencesServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StringOccurrencesService.WebClient
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonCountOccurrences_Click(object sender, EventArgs e)
+        {
+            string source = TextBoxSourceString.Text.Trim();
+            string target = TextBoxTargetString.Text.Trim();
+
+            StringOccurrencesServiceClient client = new StringOccurrencesServiceClient();
+            int occurrences = client.GetOccurrencesAsync(source, target).Result;
+            LabelOccurrences.Text = string.Format(
+                "\"{0}\" occurs {1} time(s) in \"{2}\".",
+                source,
+                occurrences,
+                target);
+        }
+    }
+}
